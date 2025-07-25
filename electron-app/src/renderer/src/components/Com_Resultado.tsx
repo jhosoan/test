a75aadb85e448de834b51857af1d79907cf04a71
jhosoan <jhosoancordeiro17@gmail.com>
2025-07-25 15:50:20 -0400

    tela de resultado fluindo

--- electron-app/src/renderer/src/components/Com_Resultado.tsx
+++ electron-app/src/renderer/src/components/Com_Resultado.tsx
@@ -6,7 +6,47 @@ export function Com_Resultado(){
             <h1>box 2</h1>
          </div>
          <div className="h-132 bg-blue-800">
-            <h1>Resultado</h1>
+            <div className="grid grid-flow-col grid-rows-3 gap-4">
+                <div className="row-span-3 ... bg-amber-300 w-200 h-120">01
+                  <div className="flex ...">
+                    <div className="size-14 grow-3 ...">01</div>
+                    <div className="size-14 grow-7 ...">02</div>
+                    <div className="size-14 grow-3 ...">03</div>
+                  </div>
+                  <div className="flex ...">
+                    <div className="size-14 grow-3 ...">01</div>
+                    <div className="size-14 grow-7 ...">02</div>
+                    <div className="size-14 grow-3 ...">03</div>
+                  </div>
+                  <div className="flex ...">
+                    <div className="size-14 grow-3 ...">01</div>
+                    <div className="size-14 grow-7 ...">02</div>
+                    <div className="size-14 grow-3 ...">03</div>
+                  </div>
+                  <div className="flex ...">
+                    <div className="size-14 grow-3 ...">01</div>
+                    <div className="size-14 grow-7 ...">02</div>
+                    <div className="size-14 grow-3 ...">03</div>
+                  </div>
+                  <div className="flex ...">
+                    <div className="size-14 grow-3 ...">01</div>
+                    <div className="size-14 grow-7 ...">02</div>
+                    <div className="size-14 grow-3 ...">03</div>
+                  </div>
+                  <div className="flex ...">
+                    <div className="size-14 grow-3 ...">01</div>
+                    <div className="size-14 grow-7 ...">02</div>
+                    <div className="size-14 grow-3 ...">03</div>
+                  </div>
+                  <div className="flex ...">
+                    <div className="size-14 grow-3 ...">01</div>
+                    <div className="size-14 grow-7 ...">02</div>
+                    <div className="size-14 grow-3 ...">03</div>
+                  </div>
+                </div>
+                <div className="col-span-2 ...">02</div>
+                <div className="col-span-2 row-span-2 ...">03</div>
+</div>
          </div>
         <Link to="/Tradicional">Voltar</Link>
         </>
