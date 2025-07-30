c932df46716b863f50e980a35faabc9c80eb7ec8
jhosoan <jhosoancordeiro17@gmail.com>
2025-07-29 22:32:17 -0400

    box de avalicao feito

--- electron-app/src/renderer/src/components/Com_Resultado.tsx
+++ electron-app/src/renderer/src/components/Com_Resultado.tsx
@@ -7,41 +7,41 @@ export function Com_Resultado(){
          </div>
          <div className="h-132 bg-blue-800">
             <div className="grid grid-flow-col grid-rows-3 gap-4">
-                <div className="row-span-3 ... bg-amber-300 w-200 h-120">01
+                <div className="row-span-3 ...  w-200 h-120">01
                   <div className="flex ...">
-                    <div className="size-14 grow-3 ...">01</div>
-                    <div className="size-14 grow-7 ...">02</div>
-                    <div className="size-14 grow-3 ...">03</div>
+                    <div className="size-14 grow-1... bg-amber-300 h-9 m-1">01</div>
+                    <div className="size-14 bg-red-700 m-1 w-25 h-9">02</div>
+                    <div className="size-14 bg-red-700 m-1 w-25 h-9">03</div>
                   </div>
                   <div className="flex ...">
-                    <div className="size-14 grow-3 ...">01</div>
-                    <div className="size-14 grow-7 ...">02</div>
-                    <div className="size-14 grow-3 ...">03</div>
+                    <div className="size-14 grow-1... bg-amber-300 h-9 m-1">01</div>
+                     <div className="size-14 bg-red-700 m-1 w-25 h-9">02</div>
+                    <div className="size-14 bg-red-700 m-1 w-25 h-9">03</div>
                   </div>
                   <div className="flex ...">
-                    <div className="size-14 grow-3 ...">01</div>
-                    <div className="size-14 grow-7 ...">02</div>
-                    <div className="size-14 grow-3 ...">03</div>
+                    <div className="size-14 grow-1... bg-amber-300 h-9 m-1">01</div>
+                     <div className="size-14 bg-red-700 m-1 w-25 h-9">02</div>
+                    <div className="size-14 bg-red-700 m-1 w-25 h-9">03</div>
                   </div>
                   <div className="flex ...">
-                    <div className="size-14 grow-3 ...">01</div>
-                    <div className="size-14 grow-7 ...">02</div>
-                    <div className="size-14 grow-3 ...">03</div>
+                    <div className="size-14 grow-1... bg-amber-300 h-9 m-1">01</div>
+                     <div className="size-14 bg-red-700 m-1 w-25 h-9">02</div>
+                    <div className="size-14 bg-red-700 m-1 w-25 h-9">03</div>
                   </div>
                   <div className="flex ...">
-                    <div className="size-14 grow-3 ...">01</div>
-                    <div className="size-14 grow-7 ...">02</div>
-                    <div className="size-14 grow-3 ...">03</div>
+                    <div className="size-14 grow-1... bg-amber-300 h-9 m-1">01</div>
+                     <div className="size-14 bg-red-700 m-1 w-25 h-9">02</div>
+                    <div className="size-14 bg-red-700 m-1 w-25 h-9">03</div>
                   </div>
                   <div className="flex ...">
-                    <div className="size-14 grow-3 ...">01</div>
-                    <div className="size-14 grow-7 ...">02</div>
-                    <div className="size-14 grow-3 ...">03</div>
+                    <div className="size-14 grow-1... bg-amber-300 h-9 m-1">01</div>
+                     <div className="size-14 bg-red-700 m-1 w-25 h-9">02</div>
+                    <div className="size-14 bg-red-700 m-1 w-25 h-9">03</div>
                   </div>
                   <div className="flex ...">
-                    <div className="size-14 grow-3 ...">01</div>
-                    <div className="size-14 grow-7 ...">02</div>
-                    <div className="size-14 grow-3 ...">03</div>
+                    <div className="size-14 grow-1... bg-amber-300 h-9 m-1">01</div>
+                     <div className="size-14 bg-red-700 m-1 w-25 h-9">02</div>
+                    <div className="size-14 bg-red-700 m-1 w-25 h-9">03</div>
                   </div>
                 </div>
                 <div className="col-span-2 ...">02</div>
